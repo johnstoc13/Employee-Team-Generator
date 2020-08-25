3f56eb18e3b3cbafe86db5dbc3ec8c6d7ed8e3d2
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-08-24 22:34:54 -0400

    duplicate id/email entry validation complete!

--- app.js
+++ app.js
@@ -20,13 +20,13 @@ const employees = [];
 
 // ********* MY NOTES ****************
 // 
-// console.log("Please build your team")          
+// console.log("Please build your team")          DONE
 // Need to prompt user for manager 1st            
-//     then create new manager with all info and push into array
-// Then prompt for other employees from questions.js
-// If intern, else if engineer, else "I don't want to add any more team members."
-// Will need an array for all employee objects....   
-// Need to validate info, to ensure not used before
+//     then create new manager with all info and push into array    DONE
+// Then prompt for other employees from questions.js                 DONE
+// If intern, else if engineer, else "I don't want to add any more team members."     DONE
+// Will need an array for all employee objects....   DONE
+// Need to validate info, to ensure not used before   DONE
 
 // Prompt user for questions
 const promptQuestions = (type) => {
@@ -37,10 +37,14 @@ const promptQuestions = (type) => {
 const startGenerator = () => {
     console.log("Please build your team");
     promptQuestions("manager").then((employee) => {
-        const newEmployee = new Manager(employee.name, employee.id, employee.email, employee.office);
-        employees.push(newEmployee);
-        // console.log(employees);
-        addNewEmployee();
+        if (!employee.name || ! employee.id || !employee.email || !employee.office) {
+            console.log("You must enter all of your Manager's information!");
+        } else {
+            const newEmployee = new Manager(employee.name, employee.id, employee.email, employee.office);
+            employees.push(newEmployee);
+            // console.log(employees[0].name);
+            addNewEmployee();
+        }
     });
 };
 
@@ -48,19 +52,35 @@ const addNewEmployee = () => {
     promptQuestions("question").then((result) => {
         if (result.question === "Intern") {
             promptQuestions("intern").then((intern) => {
-                const newIntern = new Intern(intern.name, intern.id, intern.email, intern.school);
-                employees.push(newIntern);
-                addNewEmployee();
+                if (!intern.name || !intern.id || !intern.email || !intern.school) {
+                    console.log("You must enter all of your Intern's information!");
+                } else if (intern.id == employees[0].id) {
+                    console.log("Intern cannot have the same ID as the Manager!");
+                } else if (intern.email == employees[0].email) {
+                    console.log("Intern cannot have the same email as the Manager!");
+                } else {
+                    const newIntern = new Intern(intern.name, intern.id, intern.email, intern.school);
+                    employees.push(newIntern);
+                    addNewEmployee();
+                }
             });
         } else if (result.question === "Engineer") {
             promptQuestions("engineer").then((engineer) => {
+                if (!engineer.name || !engineer.id || !engineer.email || !engineer.school) {
+                    console.log("You must enter all of your Engineer's information!");
+                } else if (engineer.id == employees[0].id) {
+                    console.log("Engineer cannot have the same ID as the Manager!");
+                } else if (engineer.email == employees[0].email) {
+                    console.log("Engineer cannot have the same email as the Manager!");
+                } else {
                 const newEngineer = new Engineer(engineer.name, engineer.id, engineer.email, engineer.github);
                 employees.push(newEngineer);
                 addNewEmployee();
+                }
             });
         } else {
             console.log("Employee additions complete!");
-            console.log(employees);
+            // console.log(employees[0].name);
         }
     });
 };
@@ -73,6 +93,11 @@ startGenerator();
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
+
+// render(employees); {
+//     console.log(render(employees));
+// };
+
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
