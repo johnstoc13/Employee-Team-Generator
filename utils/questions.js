ea7d61354a19f983130c687920b25729ef1db01a
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-08-29 09:51:16 -0400

    refactored validations

--- utils/questions.js
+++ utils/questions.js
@@ -1,23 +1,107 @@
+// Empty arrays to check for duplication
+const ids = [];
+const emails = [];
+const offices = [];
+const githubs = [];
+
+// Credit:  https://www.npmjs.com/package/inquirer
+// Credit:  https://stackoverflow.com/questions/35499782/how-to-accept-space-in-regex
+// Validation functions
+const validateName = (name) => {
+    const namePattern = /^[a-zA-Z]+(?:\s[a-zA-Z]+)?$/;
+    if (!name) {
+        return "Please fill out all fields";
+    }
+    else if (!namePattern.test(name)) {
+        return "Name must be letters only"
+    }
+    return true;
+}
+
+const validateId = (id) => {
+    const idPattern = /^[0-9]{3,10}$/;
+    if (!id) {
+        return "Please enter an ID";
+    }
+	else if (ids.includes(id)) {
+		return "ID already used, please enter a unique ID";
+    }
+    else if (!idPattern.test(id)) {
+        return "ID must be 3-10 numbers in length"
+    }
+	return true;
+}
+
+const validateEmail = (email) => {
+    const emailPattern = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}/i
+    if (!email) {
+        return "Please enter an email";
+    }
+	else if (emails.includes(email)) {
+		return "Email already used, please enter a unique Email";
+    }
+    else if (!emailPattern.test(email)) {
+        return "Please enter a valid email address"
+    }
+	return true;
+}
+
+const validateOffice = (office) => {
+    const officePattern = /^[0-9]{1,5}$/;
+    if (!office) {
+        return "Please enter an office number";
+    }
+    else if (!officePattern.test(office)) {
+        return "Office number must be 1-5 numbers in length"
+    }
+	return true;
+}
+
+const validateSchool = (school) => {
+    const schoolPattern = /^[a-zA-Z\s]+$/;
+    if (!school) {
+        return "Please enter a school";
+    }
+    else if (!schoolPattern.test(school)) {
+        return "School name must be letters only"
+    }
+    return true;
+}
+
+const validateGithub = (github) => {
+    if (!github) {
+        return "Please enter a GitHub username";
+    }
+    else if (githubs.includes(github)) {
+		return "GitHub username already used";
+    }
+    return true;
+}
+
 const manager = [
     {
         type: "input",
         name: "name",
-        message: "What is your manager's name?"
+        message: "What is your manager's name?",
+        validate: validateName
     },
     {
         type: "input",
         name: "id",
-        message: "What is your manager's id?"
+        message: "What is your manager's id?",
+        validate: validateId
     },
     {
         type: "input",
         name: "email",
-        message: "What is your manager's email?"
+        message: "What is your manager's email?",
+        validate: validateEmail
     },
     {
         type: "input",
         name: "office",
-        message: "What is your manager's office number?"
+        message: "What is your manager's office number?",
+        validate: validateOffice
     },
 ];
 
@@ -25,22 +109,26 @@ const intern = [
     {
         type: "input",
         name: "name",
-        message: "What is your intern's name?"
+        message: "What is your intern's name?",
+        validate: validateName
     },
     {
         type: "input",
         name: "id",
-        message: "What is your intern's id?"
+        message: "What is your intern's id?",
+        validate: validateId
     },
     {
         type: "input",
         name: "email",
-        message: "What is your intern's email?"
+        message: "What is your intern's email?",
+        validate: validateEmail
     },
     {
         type: "input",
         name: "school",
-        message: "What is your intern's school?"
+        message: "What is your intern's school?",
+        validate: validateSchool
     },
 ];
 
@@ -48,22 +136,26 @@ const engineer = [
     {
         type: "input",
         name: "name",
-        message: "What is your engineer's name?"
+        message: "What is your engineer's name?",
+        validate: validateName
     },
     {
         type: "input",
         name: "id",
-        message: "What is your engineer's id?"
+        message: "What is your engineer's id?",
+        validate: validateId
     },
     {
         type: "input",
         name: "email",
-        message: "What is your engineer's email?"
+        message: "What is your engineer's email?",
+        validate: validateEmail
     },
     {
         type: "input",
         name: "github",
-        message: "What is your engineer's Github username?"
+        message: "What is your engineer's Github username?",
+        validate: validateGithub
     },
 ];
 
@@ -80,4 +172,4 @@ const question = [
     },
 ];
 
-module.exports = {manager, intern, engineer, question};
+module.exports = {ids, emails, offices, githubs, manager, intern, engineer, question};
